5b01d91996c8b11c3ed8aeb9b923f51ec72a9c29
Dedy Surya Prihantoro <dedy131098@gmail.com>
2020-10-01 11:30:59 +0700

    join 4 tabel

--- pages/aset/data_aset.php
+++ pages/aset/data_aset.php
@@ -19,10 +19,16 @@
                             <thead>
                                 <tr>
                                     <th>#</th>
-                                    <th>Kode Aset</th>
-                                    <th>Nama Aset</th>
+                                    <th>Kode</th>
+                                    <th>Nama</th>
                                     <th>Merk</th>
                                     <th>Tahun</th>
+                                    <th>Nilai Aset</th>
+                                    <th>Tanggal Aset</th>
+                                    <th>Spesifikasi</th>
+                                    <th>Jenis</th>
+                                    <th>Unit</th>
+                                    <th>Suplier</th>
                                     <th>AKSI</th>
                                 </tr>
                             </thead>
@@ -31,7 +37,7 @@
                                     include "conf/conn.php";
                                     $no=0;
                                     
-                                    $sql = "SELECT * FROM tb_aset ORDER BY kode_aset ASC";
+                                    $sql = "SELECT * FROM tb_aset as a INNER JOIN tb_jenis as j ON a.kode_jenis = j.kode_jenis INNER JOIN tb_unit as u ON a.kode_unit = u.kode_unit INNER JOIN tb_suplier as s ON a.kode_suplier = s.kode_suplier ORDER BY kode_aset ASC";
 
                                     $dataAset = $conn->query($sql);
 
@@ -43,6 +49,12 @@
                                             <td><?php echo $row['nama_aset'];?></td>
                                             <td><?php echo $row['merk_aset'];?></td>
                                             <td><?php echo $row['tahun_aset'];?></td>
+                                            <td><?php echo $row['nilai_aset'];?></td>
+                                            <td><?php echo $row['tanggal_aset'];?></td>
+                                            <td><?php echo $row['spesifikasi_aset'];?></td>
+                                            <td><?php echo $row['nama_jenis'];?></td>
+                                            <td><?php echo $row['nama_unit'];?></td>
+                                            <td><?php echo $row['nama_suplier'];?></td>
                                             <td>
                                                 <a href="index.php?page=ubah_aset&id=<?=$row['kode_aset'];?>" class="btn btn-success" role="button" title="Ubah Data"><i class="glyphicon glyphicon-edit"></i></a>
                                                 <a href="pages/aset/hapus_aset.php?id=<?=$row['kode_aset'];?>" class="btn btn-danger" role="button" title="Hapus Data" onclick="return confirm('Yakin ingin menghapus data ini?')"><i class="glyphicon glyphicon-trash"></i></a>
