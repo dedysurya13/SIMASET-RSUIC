c1a651ba49e8e54cf422568dd068fd3904aa5b9d
Dedy Surya Prihantoro <dedy131098@gmail.com>
2020-10-07 10:46:25 +0700

    Update tambah_jenis_proses.php

--- pages/jenis_aset/tambah_jenis_proses.php
+++ pages/jenis_aset/tambah_jenis_proses.php
@@ -2,22 +2,20 @@
 include "../../conf/conn.php";
 
 if(isset($_POST['simpan_data'])){
-    $kode_unit = $_POST['kode_unit'];
-    $nama_unit = $_POST['nama_unit'];
-    $nama_pj = $_POST['nama_pj'];
+    $kode_jenis = $_POST['kode_jenis'];
+    $nama_jenis = $_POST['nama_jenis'];
 
-    $query = $conn->prepare("INSERT INTO tb_unit (kode_unit, nama_unit, nama_pj)
-    VALUES (:kode_unit, :nama_unit, :nama_pj)");
+    $query = $conn->prepare("INSERT INTO tb_jenis (kode_jenis, nama_jenis)
+    VALUES (:kode_jenis, :nama_jenis)");
 
-    $query->bindParam(':kode_unit',$kode_unit);
-    $query->bindParam(':nama_unit',$nama_unit);
-    $query->bindParam(':nama_pj',$nama_pj);
+    $query->bindParam(':kode_jenis',$kode_jenis);
+    $query->bindParam(':nama_jenis',$nama_jenis);
 
     $query->execute();
 
     if($query->errorCode() == 0) {
         echo '<script>alert("Data Berhasil Ditambahkan.");
-        window.location.href="../../index.php?page=data_unit"</script>';
+        window.location.href="../../index.php?page=data_jenis"</script>';
     } else {
         $errors = $query->errorInfo();
         print_r($errors);
