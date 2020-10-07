76108d23c280b35996115d6bfec6823fa07f1e73
Dedy Surya Prihantoro <dedy131098@gmail.com>
2020-10-07 14:07:32 +0700

    Create tambah_suplier_proses.php

---
+++ pages/suplier/tambah_suplier_proses.php
@@ -0,0 +1,28 @@
+<?php
+include "../../conf/conn.php";
+
+if(isset($_POST['simpan_data'])){
+    $kode_suplier = $_POST['kode_suplier'];
+    $nama_suplier = $_POST['nama_suplier'];
+    $alamat_suplier = $_POST['alamat_suplier'];
+    $telp_suplier = $_POST['telp_suplier'];
+
+    $query = $conn->prepare("INSERT INTO tb_suplier (kode_suplier, nama_suplier, alamat_Suplier, telp_suplier)
+    VALUES (:kode_suplier, :nama_suplier, :alamat_suplier, :telp_suplier)");
+
+    $query->bindParam(':kode_suplier',$kode_suplier);
+    $query->bindParam(':nama_suplier',$nama_suplier);
+    $query->bindParam(':alamat_suplier',$alamat_suplier);
+    $query->bindParam(':telp_suplier',$telp_suplier);
+
+    $query->execute();
+
+    if($query->errorCode() == 0) {
+        echo '<script>alert("Data Berhasil Ditambahkan.");
+        window.location.href="../../index.php?page=data_suplier"</script>';
+    } else {
+        $errors = $query->errorInfo();
+        print_r($errors);
+    }
+}
+?>
