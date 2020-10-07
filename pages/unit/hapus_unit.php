6b51b1fd7fe7e5760644bac3794721a044318796
Dedy Surya Prihantoro <dedy131098@gmail.com>
2020-10-07 10:04:27 +0700

    Update hapus_unit.php

--- pages/unit/hapus_unit.php
+++ pages/unit/hapus_unit.php
@@ -3,22 +3,10 @@ include "../../conf/conn.php";
 $id = $_GET['id'];
 
 try{
-    $query = $conn->query("DELETE FROM tb_aset WHERE kode_aset='$id' ");
+    $query = $conn->query("DELETE FROM tb_unit WHERE kode_unit='$id' ");
     echo '<script>alert("Data Berhasil Dihapus.");
-    window.location.href="../../index.php?page=data_aset"</script>';
+    window.location.href="../../index.php?page=data_unit"</script>';
 }catch(PDOException $e){
     echo $e->getMessage();
 }
-
-
-/*
-$query = ("DELETE FROM tb_aset WHERE kode_aset='$id' ");
-
-if(!mysql_query($query)){
-    die(mysql_error);
-}else{
-    echo '<script>alert("Data Berhasil Dihapus.");
-    window.location.href="../../index.php?page=data_aset"</script>';
-}
-*/
 ?>
