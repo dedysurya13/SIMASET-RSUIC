2e5a80d7739a6b60c5ed0058993c753216dde1da
Dedy Surya Prihantoro <dedy131098@gmail.com>
2020-10-09 10:47:49 +0700

    Create 401.php

---
+++ pages/401.php
@@ -0,0 +1,24 @@
+<div class="content-wrapper">
+    <section class="content-header">
+    <h1>
+        401 | Access Unauthorized
+    </h1>
+    <ol class="breadcrumb">
+        <li><a href="index.php"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li class="active">401 Unauthorized</li>
+    </ol>
+    </section>
+
+    <section class="content">
+    <div class="error-page">
+        <h2 class="headline text-yellow"> 401</h2>
+
+        <div class="error-content">
+            <br>
+            <h3><i class="fa fa-warning text-yellow"></i> Acess Unauthorized!</h3>
+
+            <h4>Anda tidak diperbolehkan untuk mengakses halaman ini!<a href="index.php"> Kembali ke beranda.</a></h4>
+        </div>
+    </div>
+    </section>
+</div>
