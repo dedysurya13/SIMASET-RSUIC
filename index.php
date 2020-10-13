6d160798efce5e0c312d62f3d4b80d80d31eb25b
Dedy Surya Prihantoro <dedy131098@gmail.com>
2020-10-13 13:46:37 +0700

    favicon

--- index.php
+++ index.php
@@ -15,6 +15,7 @@ if(isset($_SESSION['kode_role'])==0){
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>SIMASET RSU Islam Cawas</title>
+  <link rel="icon" href="dist/img/logorsi.ico" type="image/gif" sizes="16x16">
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.6 -->
