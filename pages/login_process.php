143c123721f48dfcdc7569c2bbdb78551bde69f6
Dedy Surya Prihantoro <dedy131098@gmail.com>
2020-10-13 13:47:02 +0700

    hapus greeting login

--- pages/login_process.php
+++ pages/login_process.php
@@ -41,7 +41,7 @@ if(($row = $stmt->fetch(PDO::FETCH_ASSOC))){
     $_SESSION['kode_petugas']=$row['kode_petugas'];
     $_SESSION['nama_petugas']=$row['nama_petugas'];
 ?>
-    <script>alert("Selamat Datang <?= $row['nama_petugas']; ?> Kamu Telah Login Ke Halaman Admin.");
+    <script>//alert("Selamat Datang <?= $row['nama_petugas']; ?> Kamu Telah Login Ke Halaman Admin.");
         window.location.href="../index.php"</script>
 <?php
 }else{
