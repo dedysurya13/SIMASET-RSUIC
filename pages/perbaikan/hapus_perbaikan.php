10d7873c1ea11a245d156115412f7d375e06800a
Dedy Surya Prihantoro <dedy131098@gmail.com>
2020-11-09 11:10:42 +0700

    Create hapus_perbaikan.php

---
+++ pages/perbaikan/hapus_perbaikan.php
@@ -0,0 +1,12 @@
+<?php 
+include "../../conf/conn.php";
+$id = $_GET['id'];
+
+try{
+    $query = $conn->query("DELETE FROM tb_perbaikan_aset WHERE kode_perbaikan_aset='$id' ");
+    echo '<script>alert("Data Berhasil Dihapus.");
+    window.location.href="../../index.php?page=data_perbaikan"</script>';
+}catch(PDOException $e){
+    echo $e->getMessage();
+}
+?>
