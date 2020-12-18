9751e756eeca9c3d8461b7f1af144141b0754d2a
Dedy Surya Prihantoro <dedy131098@gmail.com>
2020-12-18 08:35:02 +0700

    Update hapus_suplier.php

--- pages/suplier/hapus_suplier.php
+++ pages/suplier/hapus_suplier.php
@@ -3,7 +3,7 @@ include "../../conf/conn.php";
 $id = $_GET['id'];
 
 try{
-    $query = $conn->query("DELETE FROM tb_suplier WHERE kode_suplier='$id' ");
+    $query = $conn->query("DELETE FROM aset_suplier WHERE kode_suplier='$id' ");
     echo '<script>alert("Data Berhasil Dihapus.");
     window.location.href="../../index.php?page=data_suplier"</script>';
 }catch(PDOException $e){
