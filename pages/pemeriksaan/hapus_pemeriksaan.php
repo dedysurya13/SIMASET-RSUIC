b5fa5ff79a0ceae274f2421fe3a545210f3a0bbc
Dedy Surya Prihantoro <dedy131098@gmail.com>
2020-12-18 08:33:29 +0700

    Update hapus_pemeriksaan.php

--- pages/pemeriksaan/hapus_pemeriksaan.php
+++ pages/pemeriksaan/hapus_pemeriksaan.php
@@ -3,7 +3,7 @@ include "../../conf/conn.php";
 $id = $_GET['id'];
 
 try{
-    $query = $conn->query("DELETE FROM tb_pemeriksaan_aset WHERE kode_pemeriksaan_aset='$id' ");
+    $query = $conn->query("DELETE FROM aset_pemeriksaan_aset WHERE kode_pemeriksaan_aset='$id' ");
     echo '<script>alert("Data Berhasil Dihapus.");
     window.location.href="../../index.php?page=data_pemeriksaan"</script>';
 }catch(PDOException $e){
