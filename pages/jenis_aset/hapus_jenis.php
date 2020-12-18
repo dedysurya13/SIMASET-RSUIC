6baab5a213f53cc9959446d2a69e2416c36ddf5a
Dedy Surya Prihantoro <dedy131098@gmail.com>
2020-12-18 08:32:27 +0700

    Update hapus_jenis.php

--- pages/jenis_aset/hapus_jenis.php
+++ pages/jenis_aset/hapus_jenis.php
@@ -3,7 +3,7 @@ include "../../conf/conn.php";
 $id = $_GET['id'];
 
 try{
-    $query = $conn->query("DELETE FROM tb_jenis WHERE kode_jenis='$id' ");
+    $query = $conn->query("DELETE FROM aset_jenis WHERE kode_jenis='$id' ");
     echo '<script>alert("Data Berhasil Dihapus.");
     window.location.href="../../index.php?page=data_jenis"</script>';
 }catch(PDOException $e){
