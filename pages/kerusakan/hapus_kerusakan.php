c07cdc9ea4c069d1d014bf8c2154742cbfe57824
Dedy Surya Prihantoro <dedy131098@gmail.com>
2020-12-18 08:32:56 +0700

    Update hapus_kerusakan.php

--- pages/kerusakan/hapus_kerusakan.php
+++ pages/kerusakan/hapus_kerusakan.php
@@ -3,7 +3,7 @@ include "../../conf/conn.php";
 $id = $_GET['id'];
 
 try{
-    $query = $conn->query("DELETE FROM tb_kerusakan_aset WHERE kode_kerusakan_aset='$id' ");
+    $query = $conn->query("DELETE FROM aset_kerusakan_aset WHERE kode_kerusakan_aset='$id' ");
     echo '<script>alert("Data Berhasil Dihapus.");
     window.location.href="../../index.php?page=data_kerusakan"</script>';
 }catch(PDOException $e){
