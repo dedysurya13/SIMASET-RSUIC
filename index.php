48ea1285fa7b9733268e5659b4c7ecea2c243eeb
Dedy Surya Prihantoro <dedy131098@gmail.com>
2020-12-21 14:50:07 +0700

    set timezone

--- index.php
+++ index.php
@@ -4,6 +4,8 @@ header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
 header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
 
+date_default_timezone_set('Asia/Jakarta');
+
 session_start();
 include "conf/conn.php";
 if(isset($_SESSION['kode_role'])==0){
