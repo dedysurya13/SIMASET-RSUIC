1f55f94c701389bc05e938896ac1d8fc9e7b7d33
Dedy Surya Prihantoro <dedy131098@gmail.com>
2020-12-26 13:55:04 +0700

    query flag kerusakan aset

--- pages/kerusakan/tindaklanjut_kerusakan_proses.php
+++ pages/kerusakan/tindaklanjut_kerusakan_proses.php
@@ -6,6 +6,7 @@ $incrementID->execute();
 $kodeTerakhir = $incrementID->fetchColumn();
 
 $tglSekarang = date("ymd");
+$jamSekarang = date("H:i");
 
 $kodeHuruf=substr($kodeTerakhir,0,2);
 $kodeTanggal=substr($kodeTerakhir,2,6);
@@ -27,11 +28,12 @@ if(isset($_POST['tindaklanjut'])){
     $kode_perbaikan_aset = $kodeBaru;
     $kode_kerusakan_aset = $_POST['kode_kerusakan_aset'];
     $tanggal_diterima = $tglSekarang;
-    $jam_diterima = $_POST['jam_lapor'];
+    $jam_diterima = $jamSekarang;
     $kode_status = '2';
     $tanggal_selesai = NULL;
     $jam_selesai = NULL;
     $uraian_perbaikan = NULL;
+
 
 
     $query = $conn->prepare("INSERT INTO aset_perbaikan_aset (kode_perbaikan_aset, kode_kerusakan_aset, tanggal_diterima, jam_diterima, kode_status)
@@ -43,16 +45,22 @@ if(isset($_POST['tindaklanjut'])){
     $query->bindParam(':jam_diterima',$jam_diterima);
     $query->bindParam(':kode_status',$kode_status);
 
-
-    $query->execute();
-
-    if($query->errorCode() == 0) {
-        echo '<script>alert("Data Berhasil Ditambahkan.");
-        window.location.href="../../index.php?page=data_perbaikan"</script>';
-    } else {
-        $errors = $query->errorInfo();
-        print_r($errors);
+    if($query->execute()){
+        //ubah flag tidak lanjuti kerusakan aset
+        $query = $conn->prepare("UPDATE aset_kerusakan_aset SET kode_flag='1' WHERE kode_kerusakan_aset=:kode_kerusakan_aset2");
+        $query->bindParam(':kode_kerusakan_aset2',$kode_kerusakan_aset);
+        $query->execute();
+        
+        if($query->errorCode() == 0) {
+            echo '<script>alert("Data Berhasil Ditambahkan.");
+            window.location.href="../../index.php?page=data_perbaikan"</script>';
+        } else {
+            $errors = $query->errorInfo();
+            print_r($errors);
+        }
     }
+
+
     //var_dump($tanggal_selesai);
     //var_dump($tanggal_selesai1);
 }
