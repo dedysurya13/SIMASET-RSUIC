217e9fbccd178a0a5f723060eda3bf0f2b7befc3
Dedy Surya Prihantoro <dedy131098@gmail.com>
2020-12-26 13:55:42 +0700

    Update hapus_perbaikan.php
    flag kerusakan aset

--- pages/perbaikan/hapus_perbaikan.php
+++ pages/perbaikan/hapus_perbaikan.php
@@ -1,12 +1,35 @@
-<?php 
-include "../../conf/conn.php";
-$id = $_GET['id'];
 
-try{
-    $query = $conn->query("DELETE FROM aset_perbaikan_aset WHERE kode_perbaikan_aset='$id' ");
-    echo '<script>alert("Data Berhasil Dihapus.");
-    window.location.href="../../index.php?page=data_perbaikan"</script>';
-}catch(PDOException $e){
-    echo $e->getMessage();
+<?php
+include "../../conf/conn.php";
+
+if(isset($_POST['hapusperbaikan'])){
+    $kode_perbaikan_aset = $_POST['kode_perbaikan_aset'];
+    $kode_kerusakan_aset = $_POST['kode_kerusakan_aset'];
+
+
+
+    $query = $conn->prepare("DELETE FROM aset_perbaikan_aset WHERE kode_perbaikan_aset=:kode_perbaikan_aset");
+
+    $query->bindParam(':kode_perbaikan_aset',$kode_perbaikan_aset);
+
+    if($query->execute()){
+        //ubah flag tidak lanjuti kerusakan aset
+        $query = $conn->prepare("UPDATE aset_kerusakan_aset SET kode_flag='0' WHERE kode_kerusakan_aset=:kode_kerusakan_aset2");
+        $query->bindParam(':kode_kerusakan_aset2',$kode_kerusakan_aset);
+        $query->execute();
+        
+        if($query->errorCode() == 0) {
+
+            echo '<script>alert("Data Berhasil Dihapus.");
+            window.location.href="../../index.php?page=data_perbaikan"</script>';
+        } else {
+            $errors = $query->errorInfo();
+            print_r($errors);
+        }
+    }
+
+
+    //var_dump($tanggal_selesai);
+    //var_dump($tanggal_selesai1);
 }
 ?>
